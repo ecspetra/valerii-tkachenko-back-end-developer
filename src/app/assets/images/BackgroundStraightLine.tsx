f5812a3eabb975323b5dccedea65f3dad3b17eea
yuliiatkachenko <ecspetra@gmail.com>
2024-02-20 15:39:39 -0800

    Adding fixes

--- src/app/assets/images/BackgroundStraightLine.tsx
+++ src/app/assets/images/BackgroundStraightLine.tsx
@@ -15,7 +15,6 @@ const BackgroundStraightLine: FC<PropsType> = ({
 			className={classNames(`absolute`, className)}
 			width={isHorizontal ? '100%' : '1'}
 			height={isHorizontal ? '1' : '100%'}
-			viewBox='0 0 100% 100%'
 			xmlns='http://www.w3.org/2000/svg'
 		>
 			<defs>
--- src/app/components/Button/CloseButton/index.tsx
+++ src/app/components/Button/CloseButton/index.tsx
@@ -7,27 +7,24 @@ type PropsType = {
 	toggleIsMenuHidden: () => void
 }
 
-const CloseButton: FC<PropsType> = ({ isMenuHidden, toggleIsMenuHidden }) => {
-	console.log(isMenuHidden)
-	return (
-		<button
-			className='w-8 h-8 md:hidden !text-2xl duration-300 relative z-40'
-			onClick={toggleIsMenuHidden}
-		>
-			<FontAwesomeIcon
-				icon={faBarsStaggered}
-				className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 duration-300 ${
-					isMenuHidden ? 'opacity-100' : 'opacity-0'
-				}`}
-			/>
-			<FontAwesomeIcon
-				icon={faXmark}
-				className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 duration-300 text-background-color ${
-					!isMenuHidden ? 'opacity-100' : 'opacity-0'
-				}`}
-			/>
-		</button>
-	)
-}
+const CloseButton: FC<PropsType> = ({ isMenuHidden, toggleIsMenuHidden }) => (
+	<button
+		className='w-8 h-8 md:hidden !text-2xl duration-300 relative z-40'
+		onClick={toggleIsMenuHidden}
+	>
+		<FontAwesomeIcon
+			icon={faBarsStaggered}
+			className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 duration-300 ${
+				isMenuHidden ? 'opacity-100' : 'opacity-0'
+			}`}
+		/>
+		<FontAwesomeIcon
+			icon={faXmark}
+			className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 duration-300 text-background-color ${
+				!isMenuHidden ? 'opacity-100' : 'opacity-0'
+			}`}
+		/>
+	</button>
+)
 
 export default CloseButton
