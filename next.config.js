43ba147f4afd2681bee7c545997ea81374df578e
yuliiatkachenko <ecspetra@gmail.com>
2024-03-02 20:44:26 -0800

    Updated button link and next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: 'export',
+	reactStrictMode: true,
+	optimizeFonts: true,
+	images: {
+		unoptimized: true,
+	},
+}
+
+export default nextConfig
