68c942caa301bb831fd6c59a8477eaadc95ce452
yuliiatkachenko <ecspetra@gmail.com>
2024-03-02 21:05:56 -0800

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
 	reactStrictMode: true,
 	optimizeFonts: true,
-	images: {
-		unoptimized: true,
-	},
 }
 
 export default nextConfig
